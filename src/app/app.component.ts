f4d17af3d30ae91499c57823c5401108eb479bfa
treassavincy <treassavincyvalsan@gmail.com>
2022-11-21 21:50:08 +0530

    crypto commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CurrencyService } from './service/currency.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'crypto';
+ selectedCurrency:string='INR'
+
+ constructor(private currencyService:CurrencyService){}
+ sendCurrency(event:string){
+  this.currencyService.setCurrency(event)
+ }
 }
